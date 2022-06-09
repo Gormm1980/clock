61719fbb0c0332bca24170b6ca91450c4af9c397
votor79 <votor79@gmail.com>
2022-06-09 11:54:33 +0200

    actualizacion clock

--- src/Clock/Clock.jsx
+++ src/Clock/Clock.jsx
@@ -10,13 +10,23 @@ function Clock() {
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    let hours = (date.getHours() < 10 ? '0' : '') + date.getHours();
+    let minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+    let seconds = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
 
     return (
+        
         <div className="Clock">
-
+            <div className="ClockMsg">
+                <p>Bienvenido a tu oficina ...</p>
+            </div>
+            <div className="ClockHour">
+                {hours}:{minutes}
+            </div>
+            <div className="ClockTemp">
+                <div></div>
+                <div></div>
+                </div>
             <h1>Clock</h1>
             {day}/{month}/{year}
             <br />
