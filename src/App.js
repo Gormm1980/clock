f8d8ce3bab35170b5a059afdaec29a76e58ead51
gormm1980 <gormm1980@gmail.com>
2022-06-09 12:52:55 +0200

    add Calendar to  App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Clock from './Clock/Clock';
 import WidgetWeather from './Weather/Weather';
+import CalendarShow from './Calendar/Calendar';
 
 function App() {
   return (
     <div className="App">
       <Clock />
       <WidgetWeather />
+      <CalendarShow />
      
        
     </div>
